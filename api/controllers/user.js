173bd54f7c92df1239d826a3622f2a10e8995ec9
Rama Muhammad Murshal <rmurshal@gmail.com>
2022-04-21 00:51:10 +0700

    Add pembelian token

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -17,36 +17,48 @@ exports.userSignup = (req, res, next) => {
       } else {
         if (req.body.konfirmasiPassword) {
           if (req.body.password === req.body.konfirmasiPassword) {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-              if (err) {
-                res.status(500).json({
-                  message: err,
+            if (req.body.konfirmasiPin) {
+              if (req.body.pin === req.body.konfirmasiPin) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                  if (err) {
+                    res.status(500).json({
+                      message: err,
+                    });
+                  } else {
+                    const user = new User({
+                      _id: new mongoose.Types.ObjectId(),
+                      name: req.body.name,
+                      email: req.body.email,
+                      noTelp: req.body.noTelp,
+                      password: hash,
+                      saldo: 0,
+                      pin: req.body.pin
+                    });
+
+                    user
+                      .save()
+                      .then((result) => {
+                        res.status(201).json({
+                          message: "User created",
+                        });
+                      })
+                      .catch((err) => {
+                        res.status(500).json({
+                          error: err,
+                        });
+                      });
+                  }
                 });
               } else {
-                const user = new User({
-                  _id: new mongoose.Types.ObjectId(),
-                  name: req.body.name,
-                  email: req.body.email,
-                  noTelp: req.body.noTelp,
-                  password: hash,
-                  saldo: 0,
-                  pin: req.body.pin
-                });
-
-                user
-                  .save()
-                  .then((result) => {
-                    res.status(201).json({
-                      message: "User created",
-                    });
-                  })
-                  .catch((err) => {
-                    res.status(500).json({
-                      error: err,
-                    });
-                  });
+                res.status(401).json({
+                  message: "Pin confirmation failed",
+                }); 
               }
-            });
+            } else {
+              res.status(401).json({
+                message: "Pin confirmation failed",
+              });
+            }
           } else {
             res.status(401).json({
               message: "Password confirmation failed",
@@ -54,7 +66,7 @@ exports.userSignup = (req, res, next) => {
           }
         } else {
           res.status(404).json({
-            message: "Please insert password confirmation",
+            message: "Password confirmation is required",
           });
         }
       }
@@ -277,3 +289,89 @@ exports.userBayarListrik = (req, res, next) => {
     });
   }
 }
+
+exports.userCekToken = (req, res, next) => {
+  if (req.body.idPelanggan) {
+    TokenRumah.findById(req.body.idPelanggan)
+      .select("namaPemilik alamatRumah sisaToken")
+      .exec()
+      .then(tokenRumah => {
+        if (tokenRumah.sisaToken < 5000) {
+          res.status(200).json({
+            message: "Please refill yout token!",
+            notes: "Nominal purchase of tokens that can be purchased: 20000, 50000, 100000",
+            data: tokenRumah,
+          });
+        } else {
+          res.status(200).json({
+            notes: "Nominal purchase of tokens that can be purchased: 20000, 50000, 100000",
+            data: tokenRumah
+          });
+        }
+      })
+      .catch(err => res.status(500).json({ error: err }))
+  } else {
+    res.status(400).json({
+      message: "Field idPelanggan is required",
+    });
+  }
+};
+
+const nominalTokenTopup = [20000, 50000, 100000];
+
+exports.userBeliToken = (req, res, next) => {
+  const userId = req.userData._id;
+
+  if (req.body.idPelanggan && req.body.pin && req.body.nominal) {
+    const isNominalTrue = nominalTokenTopup.includes(parseInt(req.body.nominal));
+    if (isNominalTrue) {
+      User.findById(userId)
+        .exec()
+        .then(user => {
+          TokenRumah.findById(req.body.idPelanggan)
+            .exec()
+            .then(tokenRumah => {
+              if (tokenRumah) {
+                let saldoUser = user.saldo;
+                if (saldoUser >= req.body.nominal) {
+                  if (req.body.pin == user.pin) {
+                    let tokenBaru = tokenRumah.sisaToken + parseInt(req.body.nominal)
+                    TokenRumah.findByIdAndUpdate(req.body.idPelanggan, { "sisaToken": tokenBaru }, { new: true })
+                      .exec()
+                      .then(result => {
+                        saldoUser = saldoUser - parseInt(req.body.nominal);
+                        User.findByIdAndUpdate(userId, { "saldo": saldoUser }, { new: true })
+                          .exec()
+                          .then(final => {
+                            res.status(201).json({
+                              message: "Payment for token listrik is successfully",
+                            });
+                          })
+                          .catch(err => res.status(500).json({ error: err }))
+                      })
+                      .catch(err => res.status(500).json({ error: err }))
+                  } else {
+                    res.status(400).json({ message: "Pin confirmation failed" });
+                  }
+                } else {
+                  res.status(400).json({ message: "Your balance is not enough, please topup for do this transaction" }); 
+                }
+              } else {
+                res.status(404).json({ message: "No valid entry found for that ID pelanggan" });
+              }
+            })
+        })
+        .catch(err => {
+          res.status(500).json({ error: err })
+        })
+    } else {
+      res.status(400).json({
+        message: "Field nominal can only contain one of 20000, 50000, 100000",
+      });  
+    }
+  } else {
+    res.status(400).json({
+      message: "Field idPelanggan, pin, nominal is required",
+    });
+  }
+};
--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -9,5 +9,7 @@ router.get("/cekSaldo", checkAuth, userController.userGetSaldo);
 router.post("/topup", checkAuth, userController.userTopup);
 router.post("/cekPembayaranListrik", checkAuth, userController.userCekBayarListrik);
 router.post("/pembayaranListrik", checkAuth, userController.userBayarListrik);
+router.post("/cekTokenListrik", checkAuth, userController.userCekToken);
+router.post("/pembelianTokenListrik", checkAuth, userController.userBeliToken);
 
 module.exports = router;
